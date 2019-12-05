9c8736bcc8a1fbd5390ea24c61086b373d99434c
j712 <godyousama@icloud.com>
2019-12-05 14:59:17 +0900

    暫定完成版

--- func.php
+++ func.php
@@ -1,43 +1,33 @@
 <?php 
-
-$recessive = ["Albino","VPI_Axanthic"];
+$recessive = ["Albino"];
+$co_dom = ["Pastel","YellowBelly"];
+$dom = ["pin_stripe"];
 $co_dom_aka = [["YellowBelly","Ivory"],["BlackPastel","EightBall"]];
-
 $response = 
 [
 	["BlackPastel","het Albino"],
-	["Albino"],
+	["YellowBelly","het Albino"]
 ];
+//--------------遺伝子情報の分解する箇所
+//male
+foreach ($response[0] as $value) $male[] = disassembly($value, $recessive);
+if(count($male) >= 2) $males = combine($male);
+//female
+foreach ($response[1] as $value) $female[] = disassembly($value, $recessive);
+if(count($female) >= 2) $females = combine($female);
+//全件一致を行う
+$morphArr = combine([$males,$females]);
 
-//遺伝子情報の分解する箇所
-for($i=0;$i<=1;$i++){
-	//オスメスで利用するので初期化
-	$sex = [];
-	//遺伝子情報を分解する
-	foreach ($response[$i] as $value) $sex[] = disassembly($value, $recessive);
-	if(count($sex[$i]) >= 2){
-		$sexs[] = combine($sex[$i]);
-	}else{
-		$sexs[] = $sex[$i];
-	}
-	
-}
-vd($sexs);
-
-
-//出力
-$morphArr = combine([$sexs[0],$sexs[1][0]]);
-
-//文字列に変換&文字列内でsort
+//---------------文字列に変換&文字列内でsort
 foreach ($morphArr as $values) {
 	//文字列内のsort
 	natsort($values);
-	vd(array_count_values($values));
+	//個数別で出力
 	$arr = array_count_values($values);
+	//vd($arr);
 	$str = "";
 	//文字列連結を行う
 	foreach ($arr as $key => $value) {
-		$key = $key;
 		if($value == 2 && $value != ""){
 			if(!strpos($key, "het")) $key = str_replace('het ', '', $key);
 		}
@@ -46,17 +36,19 @@ foreach ($morphArr as $values) {
 	}
 	$morphs[] = trim($str);
 }
-//出力順のやつ
+
+
+
+
+
+
+
+
 array_multisort( array_map( "strlen", $morphs ), SORT_DESC, $morphs ) ;
-
 vd($morphs);
-
-//遺伝子情報を、計算するために分解（het,劣性等も含めて
 function disassembly($str,$recessive){
 	$superExplode = explode(" ", $str);
 	if(in_array($str, $recessive)){
-		echo "hetだよ";
-		echo "het ". $str,"het ". $str;
 		return $result = ["het ". $str,"het ". $str];
 	}else if(1 < count($superExplode) && $superExplode[0] == "Super"){
 		return $result = [$superExplode[1],$superExplode[1]];
@@ -64,15 +56,12 @@ function disassembly($str,$recessive){
 		return $result = [$str, ""];
 	}
 }
-
-//全権一致の関数
 function combine($elements, $result=[]) {
 	//再帰している場合はfalseの方
     $a = empty($result) ? array_shift($elements) : $result;
     //初期化
     $result = []; 
 	$b = array_shift($elements);
-
     foreach($a as $val1) {
         foreach ($b as $val2) $result[] = array_merge((array)$val1, (array)$val2);
     }
@@ -81,10 +70,10 @@ function combine($elements, $result=[]) {
     return $result;
 }
 
+//出力用
 function vd($arr){
 	echo "<pre>";
 	var_export($arr);
-	echo "</pre><br>";
+	echo "</pre>";
 }
-
 ?>
